b1429b230257a84d127fed6598c211d84bd9fc7b
ViktorSvertoka <victor.svertoka@gmail.com>
2025-03-25 22:25:35 +0200

    feat(files): change styles

--- src/js/markup.js
+++ src/js/markup.js
@@ -3,22 +3,27 @@ import editLogo from "../images/pause.svg";
 import removeLogo from "../images/cross.svg";
 
 export function createMarkup({ id, description, priority, done }) {
+  const priorityClass =
+    {
+      Easy: "todo__priority--green",
+      Normal: "todo__priority--yellow",
+      Hard: "todo__priority--red",
+    }[priority] || "";
+
   return `
-    <li class="todo__item ${
-      done ? "todo__item--bg-green" : ""
-    }" data-task-id="${id}">
+    <li class="todo__item ${priorityClass}" data-task-id="${id}">
       <div class="todo__group">
         <article class="todo__description ${done ? "todo__text--done" : ""}">
           ${description}
         </article>
-        <span class="todo__priority ${done ? "todo__text--done" : ""}">
+        <span class="todo__priority ${priorityClass}">
           ${priority}
         </span>
       </div>
       <div class="todo__actions">
-        <img class="todo__svg done" src="${doneLogo}" alt="Check button" />
-        <img class="todo__svg edit" src="${editLogo}" alt="Pause button" />
-        <img class="todo__svg remove" src="${removeLogo}" alt="Cross button" />
+        <img class="todo__svg done" src="${doneLogo}" alt="Check button" title="Done task" />
+        <img class="todo__svg edit" src="${editLogo}" alt="Pause button" title="Edit task" />
+        <img class="todo__svg remove" src="${removeLogo}" alt="Cross button" title="Remove task" />
       </div>
     </li>`;
 }
